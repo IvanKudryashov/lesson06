088fb390ad5f66b75770c069e8c381fb90612e89
legolav <bbabkova4@gmail.com>
2022-10-04 22:32:58 +0300

    добавлена задача 41

---
+++ zadachi/Program.cs
@@ -0,0 +1,31 @@
+﻿void zadacha41()
+{
+    //Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    Console.WriteLine("Введите число M:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[m];
+    int count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    PrintArray(array);
+    for (int i = 0; i < m; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    Console.WriteLine($"Чисел больше нуля: {count}");
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+zadacha41();
