c66072111da680bf41059cfafdaa0ed19f0b054d
legolav <bbabkova4@gmail.com>
2022-10-05 14:43:23 +0300

    добавлена задача 43

--- zadachi/Program.cs
+++ zadachi/Program.cs
@@ -28,4 +28,29 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
+void zadacha43()
+{
+    //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+    //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+    Console.WriteLine("Введите число b1:");
+    double b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число k1:");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число b2:");
+    double b2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число k2:");
+    double k2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}");
+    if (k1 == k2)
+        Console.WriteLine("Прямые параллельны");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"({Math.Round(x,2)}, {Math.Round(y,2)})");
+    }
+}
+
 zadacha41();
+//zadacha43();
